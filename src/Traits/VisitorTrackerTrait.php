e914203952751728fedfd2c46f2abf324e87693c
rabie85 <dif.rabie@gmail.com>
2022-04-24 14:03:01 +0100

    put functions of model in trait

--- src/Traits/VisitorTrackerTrait.php
+++ src/Traits/VisitorTrackerTrait.php
@@ -3,5 +3,18 @@
 namespace Drsoft\VisitorTracker\Traits;
 
 trait VisitorTrackerTrait{
+    public function scopeVisitorsWithinSeconds($query, $seconds)
+    {
+        
+        return $query->where('created_at', '>=', now()->subSeconds($seconds));
+    }
 
+    public function scopeVisitorsWithinMinutes($query, $minutes)
+    {
+        return $query->visitorsWithinSeconds($minutes * 60);
+    }
+    public function scopeVisitorsWithinHours($query, $hours)
+    {
+        return $query->visitorsWithinSeconds($hours * 60 * 60);
+    }
 }
