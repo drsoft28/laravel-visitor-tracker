cd9ef7d3f6717533536054f71935fad17fa2b859
drsoft28 <dif.rabie@gmail.com>
2025-03-09 13:55:09 +0100

    update namespace Model Sub

---
+++ src/Subs/VisitorTracker.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Drsoft28\VisitorTracker\Traits\VisitorTrackerTrait;
+class VisitorTracker extends Model
+{
+    use VisitorTrackerTrait;
+
+    
+}
